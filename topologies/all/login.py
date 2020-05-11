989ebd4e5331db80a0df8a4842cd29d966e95657
BG <bmgreer09@gmail.com>
2020-05-11 14:43:25 -0400

    testing main menu with re-written configure topo

--- topologies/all/login.py
+++ topologies/all/login.py
@@ -4,12 +4,22 @@ import sys
 import signal
 import re
 from ruamel.yaml import YAML
-from deploytopology import DeployTopology as deploy
+import json
+from datetime import timedelta, datetime, timezone, date
+from ruamel.yaml import YAML
+import getopt
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+from rcvpapi.rcvpapi import *
+import logging
+import time
+
 
 
 ######################################
 ########## Global Variables ##########
 ######################################
+DEBUG = False
 
 # Open ACCESS_INFO.yaml and load the variables
 f = open('/etc/ACCESS_INFO.yaml')
@@ -56,6 +66,152 @@ def sort_veos(vd):
   for t_veos in tmp_l:
     fin_l.append(tmp_d[t_veos])
   return(fin_l)
+
+def remove_configlets( client, device, lab_configlets):
+    """
+    Removes all configlets except the ones defined here or starting with SYS_
+    Define base configlets that are to be untouched
+    mext = lab type to keep track of which base configlets to keep.  Added for RATD and RATD-Ring
+    """
+    base_configlets = ['ATD-INFRA']
+    
+    configlets_to_remove = []
+    configlets_to_remain = base_configlets
+
+    configlets = client.getConfigletsByNetElementId(device)
+    for configlet in configlets['configletList']:
+        if configlet['name'] not in base_configlets or configlet['name'] not in lab_configlets:
+            configlets_to_remove.append(configlet['name'])
+            send_to_syslog("INFO", "Configlet {0} not part of base on {1} - Removing from device".format(configlet['name'], device.hostname))
+        elif configlet['name'] in base_configlets:
+            configlets_to_remain.append(configlet['name'])
+            send_to_syslog("INFO", "Configlet {0} is part of the base on {1} - Configlet will remain.".format(configlet['name'], device.hostname))
+        else:
+            pass
+    device.removeConfiglets(client, configlets_to_remove)
+    client.addDeviceConfiglets(device, configlets_to_remain)
+    client.applyConfiglets(device)
+
+def get_device_info( client):
+    eos_devices = []
+    for dev in client.inventory:
+        tmp_eos = client.inventory[dev]
+        tmp_eos_sw = CVPSWITCH(dev, tmp_eos['ipAddress'])
+        tmp_eos_sw.updateDevice(client)
+        eos_devices.append(tmp_eos_sw)
+    return(eos_devices)
+
+
+def update_topology(client, lab, configlets):
+    # Get all the devices in CVP
+    devices = get_device_info(client)
+    # Loop through all devices
+    # for device in devices:
+    for device in devices:
+        # Get the actual name of the device
+        device_name = device.hostname
+        
+        # Define a list of configlets built off of the lab yaml file
+        lab_configlets = []
+        for configlet_name in configlets[lab][device_name]:
+            lab_configlets.append(configlet_name)
+
+        # Remove unnecessary configlets
+        remove_configlets(client, device, lab_configlets)
+
+        # Apply the configlets to the device
+        client.addDeviceConfiglets(device, lab_configlets)
+        client.applyConfiglets(device)
+
+    # Perform a single Save Topology by default
+    client.saveTopology()
+
+def send_to_syslog(mstat,mtype):
+    """
+    Function to send output from service file to Syslog
+    Parameters:
+    mstat = Message Status, ie "OK", "INFO" (required)
+    mtype = Message to be sent/displayed (required)
+    """
+    mmes = "\t" + mtype
+    logging.info("[{0}] {1}".format(mstat,mmes.expandtabs(7 - len(mstat))))
+    if DEBUG:
+        print("[{0}] {1}".format(mstat,mmes.expandtabs(7 - len(mstat))))
+
+
+def deploy_lab(selected_menu,selected_lab):
+
+    # Check for additional commands in lab yaml file
+    lab_file = open('/home/arista/menus/{0}'.format(selected_menu + '.yaml'))
+    lab_info = YAML().load(lab_file)
+    lab_file.close()
+
+    additional_commands = []
+    if 'additional_commands' in lab_info['lab_list'][selected_lab]:
+        additional_commands = lab_info['lab_list'][selected_lab]['additional_commands']
+
+    # Get access info for the topology
+    f = open('/etc/ACCESS_INFO.yaml')
+    access_info = YAML().load(f)
+    f.close()
+
+    # List of configlets
+    lab_configlets = lab_info['labconfiglets']
+
+    # Send message that deployment is beginning
+    print("Starting deployment for {0} - {1} lab...".format(selected_menu,selected_lab))
+
+    # Adding new connection to CVP via rcvpapi
+    cvp_clnt = ''
+    for c_login in access_info['login_info']['cvp']['shell']:
+        if c_login['user'] == 'arista':
+            while not cvp_clnt:
+                try:
+                    cvp_clnt = CVPCON(access_info['nodes']['cvp'][0]['internal_ip'],c_login['user'],c_login['pw'])
+                    send_to_syslog("OK","Connected to CVP at {0}".format(access_info['nodes']['cvp'][0]['internal_ip']))
+
+                except:
+                    send_to_syslog("ERROR", "CVP is currently unavailable....Retrying in 30 seconds.")
+                    print("CVP is currently unavailable....Retrying in 30 seconds.")
+                    time.sleep(30)
+
+    # Make sure option chosen is valid, then configure the topology
+    print("Deploying configlets for {0} - {1} lab...".format(selected_menu,selected_lab))
+    send_to_syslog("INFO", "Setting {0} topology to {1} setup".format(access_info['topology'], selected_lab))
+    update_topology(cvp_clnt, selected_lab, lab_configlets)
+    
+    # Execute all tasks generated from reset_devices()
+    print("Creating Change Control for for {0} - {1} lab...".format(selected_menu,selected_lab))
+    cvp_clnt.getAllTasks("pending")
+    tasks_to_check = cvp_clnt.tasks['pending']
+    cvp_clnt.execAllTasks("pending")
+    send_to_syslog("OK", 'Completed setting devices to topology: {}'.format(selected_lab))
+
+    print("Executing change control for {0} - {1} lab. Please wait for tasks to finish...".format(selected_menu,selected_lab))
+    all_tasks_completed = False
+    while not all_tasks_completed:
+        tasks_running = []
+        for task in tasks_to_check:
+            if cvp_clnt.getTaskStatus(task['workOrderId'])['taskStatus'] != 'Completed':
+                tasks_running.append(task)
+            elif cvp_clnt.getTaskStatus(task['workOrderId'])['taskStatus'] == 'Failed':
+                print('Task {0} failed. Please check CVP for more information'.format(task['workOrderId']))
+            else:
+                pass
+
+        if len(tasks_running) == 0:
+            
+            print("Tasks finished. Finalizing deployment for {0} - {1} lab...".format(selected_menu,selected_lab))
+
+            # Execute additional commands if there are any for the lab
+            for command in additional_commands:
+                os.system(command)
+                
+            print("Deployment for {0} - {1} lab is complete.".format(selected_menu,selected_lab))
+            all_tasks_completed = True
+        else:
+            print("{0}/{1} tasks completed. Please wait...".format(str(len(tasks_to_check) - len(tasks_running)), len(tasks_to_check)))
+        
 
 def device_menu():
     global menu_mode
